c3ba5a4805e3b07a9b33d3e1c6ac9a224bbf7377
sushant81074 <sushant94601@gmail.com>
2026-01-22 14:14:45 +0530

    full winner storeing for match odds

--- src/modules/markets/services/handlers.js
+++ src/modules/markets/services/handlers.js
@@ -20,33 +20,33 @@ const getOddsHandler = async () => {
                     if (!marketOddsPoint) return;
                     if (!Array.isArray(value) || !value.length) return;
 
-                    value.map(async ev => {
+                    value.map(async (ev) => {
                         const marketIds = await getCache(`EVENT_MARKETS:${ev}`);
 
                         if (!Array.isArray(marketIds)) return;
 
-                        marketIds.map(async m => {
+                        marketIds.map(async (m) => {
                             let odds = await getApi([marketOddsPoint, ev, m], "market");
                             if (!odds) return;
                             if (Array.isArray(odds) && odds.length == 0) {
                                 return;
                             } else if (typeof odds === "object" && Object.keys(odds.data).length === 0) {
-                                return
+                                return;
                             }
 
                             await setCache(`MARKET_ODDS:${m}`, odds);
-                            winnerSId = odds?.data?.runners?.find(r => r.status == "WINNER")?.selectionId;
+                            winnerSId = odds?.data?.runners?.find((r) => r.status == "WINNER");
                             if (winnerSId) await setCache(`win:marketOdds:${e}:${m}`, winnerSId, 24 * 60 * 60);
-                        })
-                    })
-                }))
+                        });
+                    });
+                }),
+            );
         }
-
     } catch (error) {
         console.error("error occured in getOddsHandler:", error);
     }
-}
+};
 
 module.exports = {
-    getOddsHandler
-}
+    getOddsHandler,
+};
